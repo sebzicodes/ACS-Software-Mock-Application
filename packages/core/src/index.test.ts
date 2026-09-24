1f6693dde1908cc976f830a99386424c24fb0ec7
Gabriel <gabrieldavis1901@gmail.com>
2026-09-24 13:13:39 -0700

    ran first successful vitest and learned structure

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,7 +1,10 @@
-import { describe, it, expect } from "./index.ts";
-
+import { describe, it, expect } from "vitest"
+import { numberOrNull } from "./index.js";
 describe("null", () => {
-     it("returns absent value", () => {
-          expect(value(null)).toBe("absent value");
-     });
+     it("returns absent value for null", () => {
+          expect(numberOrNull(null)).toBe("absent value");
+     })
+     it("returns string for number", () => {
+          expect(numberOrNull(5)).toBe("this is 5");
+     })
 });
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
-export 
-function numberOrNull(value: number | null): string {
+
+export function numberOrNull(value: number | null): string {
     if (value === null) {
          return "absent value";
     }
-   return "this is a " + value.toFixed();
+   return "this is " + value.toFixed();
 }
